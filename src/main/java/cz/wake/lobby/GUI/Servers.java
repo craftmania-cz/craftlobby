e82732b4f93726dc37cae4ef77c846ca27c685e8
MrWakeCZ <joseff.kral@gmail.com>
2016-12-10 19:52:34 +0100

    [1.6.71] Zobrazení online hráčů v menu

--- src/main/java/cz/wake/lobby/GUI/Servers.java
+++ src/main/java/cz/wake/lobby/GUI/Servers.java
@@ -42,31 +42,31 @@ public class Servers implements Listener {
         inv.setItem(53, colorful);
 
         ItemStack surv = ItemFactory.create(Material.GOLD_PICKAXE, (byte) 0, "§e§lSurvival", "§8Survival, Economy, PVP", "",
-                "§7Stary dobry survival, s mnoha", "§7vylepsenim a prikazy.", "", "§b▸ Kliknutim te portnu");
+                "§7Stary dobry survival, s mnoha", "§7vylepsenim a prikazy.", "", "§b▸ Kliknutim te portnu", getPlayers("survival"));
         inv.setItem(20, surv);
 
         ItemStack skyb = ItemFactory.create(Material.SAPLING, randomByte((byte) 0, (byte) 5), "§e§lSkyBlock", "§8Survival, Economy, PVP", "",
-                "§7Spawnes se na opustenem ostrove", "§7sam a se zakladnimi surovinami.", "§7Postav si svuj ostrov sam", "§7nebo s kamarady.", "", "§b▸ Kliknutim te portnu");
+                "§7Spawnes se na opustenem ostrove", "§7sam a se zakladnimi surovinami.", "§7Postav si svuj ostrov sam", "§7nebo s kamarady.", "", "§b▸ Kliknutim te portnu", getPlayers("skyblock"));
         inv.setItem(21, skyb);
 
         ItemStack crea1 = ItemFactory.create(Material.LAVA_BUCKET, (byte) 0, "§e§lCreative #1", "§8Creative, pozemky, WorldEdit", "",
-                "§7Svet ve kterem muzes prakticky", "§7delat co chces, stavet, budovat", "§7nebo se proste zlepsovat.", "", "§b▸ Kliknutim te portnu");
+                "§7Svet ve kterem muzes prakticky", "§7delat co chces, stavet, budovat", "§7nebo se proste zlepsovat.", "", "§b▸ Kliknutim te portnu", getPlayers("creative"));
         inv.setItem(22, crea1);
 
         ItemStack crea2 = ItemFactory.create(Material.WATER_BUCKET, (byte) 0, "§e§lCreative #2", "§8Creative, pozemky, WorldEdit", "",
-                "§7Dalsi svet, ve kterem", "§7lze delat prakticky vse.", "", "§b▸ Kliknutim te portnu");
+                "§7Dalsi svet, ve kterem", "§7lze delat prakticky vse.", "", "§b▸ Kliknutim te portnu", getPlayers("creative2"));
         inv.setItem(23, crea2);
 
         ItemStack pris = ItemFactory.create(Material.IRON_FENCE, (byte) 0, "§e§lPrison", "§8RankUp, Prestige, Mining", "", "§7Svet ve kterem je dulezite",
-                "§7se zlepsovat a kopat", "§7jinak budes pozadu.", "", "§b▸ Kliknutim te portnu");
+                "§7se zlepsovat a kopat", "§7jinak budes pozadu.", "", "§b▸ Kliknutim te portnu", getPlayers("prison"));
         inv.setItem(24, pris);
 
         ItemStack van = ItemFactory.create(Material.EMERALD, (byte) 0, "§e§lVanilla (1.11)", "§8Vanilla, Survival, PVP", "", "§7Otevreny svet ve kterem",
-                "§7poznas co to je zit", "§7na vlastni pest.", "", "§b▸ Kliknutim te portnu");
+                "§7poznas co to je zit", "§7na vlastni pest.", "", "§b▸ Kliknutim te portnu", getPlayers("vanilla"));
         inv.setItem(25, van);
 
         ItemStack fac = ItemFactory.create(Material.TNT, (byte) 0, "§e§lFactions", "§8Frakce, PVP, souboje", "", "§7Zde nikdy nevis", "§7kdo ti jde po krku.",
-                "§7Az to zjistis, bude pozde.", "", "§b▸ Kliknutim te portnu");
+                "§7Az to zjistis, bude pozde.", "", "§b▸ Kliknutim te portnu", getPlayers("factions"));
         inv.setItem(26, fac);
 
         ItemStack bedw = ItemFactory.create(Material.BED, (byte) 0, "§a§lBedWars", "§8Minihra, PVP", "", "§7Ctyri teamy proti sobe", "§7a jeden vitez! Bude to tvuj team?",
@@ -121,6 +121,19 @@ public class Servers implements Listener {
     private byte randomByte(byte start, byte end) {
         int cislo = start + random.nextInt(end - start + 1);
         return (byte) cislo;
+    }
+
+    private String getPlayers(final String server){
+        int pl = Main.getInstance().fetchData().getOnlinePlayers(server);
+        if(pl == 0){
+            return "§8Nikdo na serveru neni!";
+        } else if (pl == 1){
+            return "§8Aktualne hraje " + pl + " hrac.";
+        } else if (pl >= 2 && pl <= 4){
+            return "§8Aktualne hraje " + pl + " hraci.";
+        } else {
+            return "§8Aktualne hraje " + pl + " hracu.";
+        }
     }
 
 
