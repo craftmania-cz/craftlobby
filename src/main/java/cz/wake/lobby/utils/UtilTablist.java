5d67217b0152979c84c355b27a0641689cbdbab2
MrWakeCZ <joseff.kral@gmail.com>
2018-02-17 00:58:36 +0100

    Úprava tablistu

--- src/main/java/cz/wake/lobby/utils/UtilTablist.java
+++ src/main/java/cz/wake/lobby/utils/UtilTablist.java
@@ -9,6 +9,10 @@ public class UtilTablist {
             p.setPlayerListName("§3§lMAJITEL §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.hladmin")) {
             p.setPlayerListName("§c§lHL.ADMIN §f" + p.getName());
+        } else if (p.hasPermission("craftlobby.prefix.hlbuilder")) {
+            p.setPlayerListName("§1§lHL.BUILDER §f" + p.getName());
+        } else if (p.hasPermission("craftlobby.prefix.developer")) {
+            p.setPlayerListName("§e§lDEVELOPER §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.eventer")) {
             p.setPlayerListName("§d§lEVENTER §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.adminka")) {
@@ -16,9 +20,11 @@ public class UtilTablist {
         } else if (p.hasPermission("craftlobby.prefix.admin")) {
             p.setPlayerListName("§c§lADMIN §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.builder")) {
-            p.setPlayerListName("§5§lBUILDER §f" + p.getName());
+            p.setPlayerListName("§1§lBUILDER §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.helper")) {
             p.setPlayerListName("§2§lHELPER §f" + p.getName());
+        } else if (p.hasPermission("craftlobby.prefix.hero")) {
+            p.setPlayerListName("§5§lHERO §f" + p.getName());
         } else if (p.hasPermission("craftlobby.prefix.vip")) {
             p.setPlayerListName("§a§lVIP §f" + p.getName());
         } else {
