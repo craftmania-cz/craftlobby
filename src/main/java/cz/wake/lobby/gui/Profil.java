78240805ad4e5aa23c8e46c699594b8a0fc67d8b
MrWakeCZ <joseff.kral@gmail.com>
2019-01-13 03:38:44 +0100

    Úprava statistik v profilu

--- src/main/java/cz/wake/lobby/gui/Profil.java
+++ src/main/java/cz/wake/lobby/gui/Profil.java
@@ -1,5 +1,6 @@
 package cz.wake.lobby.gui;
 
+import cz.craftmania.craftcore.spigot.builders.items.ItemBuilder;
 import cz.craftmania.crafteconomy.api.CraftCoinsAPI;
 import cz.craftmania.crafteconomy.api.CraftTokensAPI;
 import cz.craftmania.crafteconomy.api.VoteTokensAPI;
@@ -45,7 +46,7 @@ public class Profil implements Listener {
 
         SkullMeta headItemMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.SKULL_ITEM);
         headItemMeta.setOwner(p.getName());
-        headItemMeta.setDisplayName("§aInformace o tobe");
+        headItemMeta.setDisplayName("§a§lInformace o tobe");
         ItemStack headItem = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
         ArrayList<String> headLore = new ArrayList<String>();
         headLore.add("");
@@ -54,49 +55,39 @@ public class Profil implements Listener {
         headLore.add("§7CraftCoins: §f" + CraftCoinsAPI.getCoins(p));
         headLore.add("§7CraftTokens: §f" + CraftTokensAPI.getTokens(p));
         headLore.add("§7VoteTokens: §f" + VoteTokensAPI.getVoteTokens(p));
-        //headLore.add("§7SkyDust: §f" + Main.getInstance().getSQL().getSkyKeysDust(p.getUniqueId()));
-        //headLore.add("§7Celkem hlasu: §f" + Main.getInstance().getSQL().getVotesAll(p.getUniqueId()));
         headLore.add("§7Web group: §f" + getWebGroup(Main.getInstance().getSQL().getPlayerProfileDataInt(p, "web_group")));
         headLore.add("§7Celkem odehrany cas: §f" + TimeUtils.formatTime("%dd, %hh %mm", Main.getInstance().getSQL().getPlayerProfileDataInt(p, "played_time"), false));
         headItemMeta.setLore(headLore);
         headItem.setItemMeta(headItemMeta);
         menu.setItem(12, headItem);
 
-        ItemStack status = ItemFactory.create(Material.PAPER, (byte) 0, "§aTvuj status", "§8Status je viditelny na webu", "§8v profilech", "", "§f" + Main.getInstance().getSQL().getPlayerProfileDataString(p, "status"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack status = ItemFactory.create(Material.PAPER, (byte) 0, "§a§lTvuj status", "§8Status je viditelny na webu", "§8v profilech", "", "§f" + Main.getInstance().getSQL().getPlayerProfileDataString(p, "status"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         menu.setItem(14, status);
 
-        ItemStack soc = ItemFactory.createHead("Test", "4ac1c429-e329-4861-b1d6-c4bde50022d9", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGViNDYxMjY5MDQ0NjNmMDdlY2ZjOTcyYWFhMzczNzNhMjIzNTliNWJhMjcxODIxYjY4OWNkNTM2N2Y3NTc2MiJ9fX0=", "§aSocialni site", "", "§7Chces vsem poskytnout prime", "§7odkazy na svem profilu?", "", "§bOdkazy se zobrazuji na webu", "§bv profilech.", "", "§eKliknutim provedes zmeny");
+        ItemStack soc = ItemFactory.createHead("Test", "4ac1c429-e329-4861-b1d6-c4bde50022d9", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGViNDYxMjY5MDQ0NjNmMDdlY2ZjOTcyYWFhMzczNzNhMjIzNTliNWJhMjcxODIxYjY4OWNkNTM2N2Y3NTc2MiJ9fX0=", "§a§lSocialni site", "", "§7Chces vsem poskytnout prime", "§7odkazy na svem profilu?", "", "§bOdkazy se zobrazuji na webu", "§bv profilech.", "", "§eKliknutim provedes zmeny");
         menu.setItem(29, soc);
 
-        ItemStack statistiky = ItemFactory.create(Material.BOOK, (byte) 0, "§aStatistiky", "", "§7Zobrazi vsechny tve statistiky", "§7ze serveru.", "", "§eKlikni pro zobrazeni");
+        ItemStack statistiky = ItemFactory.create(Material.BOOK, (byte) 0, "§a§lStatistiky", "", "§7Zobrazi vsechny tve statistiky", "§7ze serveru.", "", "§eKlikni pro zobrazeni");
         menu.setItem(30, statistiky);
 
-        ItemStack multipliers = ItemFactory.create(Material.POTION, (byte) 0, "§aMultipliery", "", "§7Chces vydelavat vic?", "§7Zde je to prave misto...", "", "§cPlanovano...");
+        ItemStack multipliers = ItemFactory.create(Material.POTION, (byte) 0, "§a§lMultipliery", "", "§7Chces vydelavat vic?", "§7Zde je to prave misto...", "", "§cPlanovano...");
         menu.setItem(32, multipliers);
 
-        ItemStack levels = ItemFactory.create(Material.BREWING_STAND_ITEM, (byte) 0, "§aCraftLevels", "", "§7Ziskavanim levelu", "§7si odemknes nove veci na serveru.", "", "§cPlanovano...");
+        ItemStack levels = ItemFactory.create(Material.BREWING_STAND_ITEM, (byte) 0, "§a§lCraftLevels", "", "§7Ziskavanim levelu", "§7si odemknes nove veci na serveru.", "", "§cPlanovano...");
         menu.setItem(31, levels);
 
-        ItemStack achievements = ItemFactory.create(Material.EMERALD, (byte) 0, "§aAchievementy", "", "§7Chces videt, ktery ukoly", "§7jsi splnil a ktere", "§7musis splnit?", "", "§cPlanovano...");
+        ItemStack achievements = ItemFactory.create(Material.EMERALD, (byte) 0, "§a§lAchievementy", "", "§7Chces videt, ktery ukoly", "§7jsi splnil a ktere", "§7musis splnit?", "", "§cPlanovano...");
         menu.setItem(33, achievements);
 
-        ItemStack questy = ItemFactory.create(Material.DIAMOND, (byte) 0, "§aQuesty", "", "§7Nuda? No tak zkus kazdy den", "§7splnit nektery z techto Questu!", "", "§cPlanovano...");
+        ItemStack questy = ItemFactory.create(Material.DIAMOND, (byte) 0, "§a§lQuesty", "", "§7Nuda? No tak zkus kazdy den", "§7splnit nektery z techto Questu!", "", "§cPlanovano...");
         menu.setItem(39, questy);
 
-        ItemStack villages = ItemFactory.create(Material.DARK_OAK_DOOR_ITEM, (byte) 0, "§aVillages", "", "§7Kdo vi.. co to bude?", "", "§cPlanovano...");
+        ItemStack villages = ItemFactory.create(Material.DARK_OAK_DOOR_ITEM, (byte) 0, "§a§lVillages", "", "§7Kdo vi.. co to bude?", "", "§cPlanovano...");
         menu.setItem(38, villages);
 
-        /*if (Main.getInstance().getSQL().isAT(p)) {
-            ItemStack i = ItemFactory.create(Material.PAINTING, (byte) 0, "§aAT Stalker", "§7Prehled tve aktivity", "§7na serveru.", "", "§eKlikni pro zobrazeni");
-            menu.setItem(42, i);
-        } else {
-            ItemStack i = ItemFactory.create(Material.BARRIER, (byte) 0, "§cAT Sekce", "§7K zobrazeni teto sekce", "§7musis byt v AT.");
-            menu.setItem(42, i);
-        } */
+        ItemStack nastaveni = ItemFactory.create(Material.REDSTONE_COMPARATOR, (byte) 0, "§a§lNastaveni uctu", "", "§7Diky nastaveni si muzes", "§7prispusobit lobby/hry podle sebe.", "", "§eKlikni pro zobrazeni/nastaveni");
 
-        ItemStack nastaveni = ItemFactory.create(Material.REDSTONE_COMPARATOR, (byte) 0, "§aNastaveni uctu", "", "§7Diky nastaveni si muzes", "§7prispusobit lobby/hry podle sebe.", "", "§eKlikni pro zobrazeni/nastaveni");
-
-        ItemStack jazyk = ItemFactory.createHead("§aNastaveni jazyka", "0ceac85e-159d-4f9d-a1c2-c8acde792f23", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjFkZDRmZTRhNDI5YWJkNjY1ZGZkYjNlMjEzMjFkNmVmYTZhNmI1ZTdiOTU2ZGI5YzVkNTljOWVmYWIyNSJ9fX0=");
+        ItemStack jazyk = ItemFactory.createHead("§a§lNastaveni jazyka", "0ceac85e-159d-4f9d-a1c2-c8acde792f23", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjFkZDRmZTRhNDI5YWJkNjY1ZGZkYjNlMjEzMjFkNmVmYTZhNmI1ZTdiOTU2ZGI5YzVkNTljOWVmYWIyNSJ9fX0=");
         ItemMeta bhMeta = jazyk.getItemMeta();
         ArrayList<String> bhLore = new ArrayList();
         bhLore.add("§7Zmen si jazyk serveru.");
@@ -120,25 +111,25 @@ public class Profil implements Listener {
 
         Inventory inv = Bukkit.createInventory(null, 45, "Socialni site");
 
-        ItemStack facebook = ItemFactory.createHead("Facebook", "4ac1c429-e329-4861-b1d6-c4bde50022d9", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGViNDYxMjY5MDQ0NjNmMDdlY2ZjOTcyYWFhMzczNzNhMjIzNTliNWJhMjcxODIxYjY4OWNkNTM2N2Y3NTc2MiJ9fX0=", "§bFacebook", "", getSocialLinks(p, "soc_facebook"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack facebook = ItemFactory.createHead("Facebook", "4ac1c429-e329-4861-b1d6-c4bde50022d9", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZGViNDYxMjY5MDQ0NjNmMDdlY2ZjOTcyYWFhMzczNzNhMjIzNTliNWJhMjcxODIxYjY4OWNkNTM2N2Y3NTc2MiJ9fX0=", "§b§lFacebook", "", getSocialLinks(p, "soc_facebook"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(10, facebook);
 
-        ItemStack twitter = ItemFactory.createHead("Twitter", "11fce6c7-71ad-464e-98e7-c8e579de4758", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzY4NWEwYmU3NDNlOTA2N2RlOTVjZDhjNmQxYmEyMWFiMjFkMzczNzFiM2Q1OTcyMTFiYjc1ZTQzMjc5In19fQ==", "§bTwitter", "", getSocialLinks(p, "soc_twitter"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack twitter = ItemFactory.createHead("Twitter", "11fce6c7-71ad-464e-98e7-c8e579de4758", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzY4NWEwYmU3NDNlOTA2N2RlOTVjZDhjNmQxYmEyMWFiMjFkMzczNzFiM2Q1OTcyMTFiYjc1ZTQzMjc5In19fQ==", "§b§lTwitter", "", getSocialLinks(p, "soc_twitter"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(12, twitter);
 
-        ItemStack youtube = ItemFactory.createHead("Youtube", "6486882a-2871-4db3-922d-98ebc3166c6b", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDJmNmMwN2EzMjZkZWY5ODRlNzJmNzcyZWQ2NDU0NDlmNWVjOTZjNmNhMjU2NDk5YjVkMmI4NGE4ZGNlIn19fQ==", "§bYoutube", "", getSocialLinks(p, "soc_ytb"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack youtube = ItemFactory.createHead("Youtube", "6486882a-2871-4db3-922d-98ebc3166c6b", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDJmNmMwN2EzMjZkZWY5ODRlNzJmNzcyZWQ2NDU0NDlmNWVjOTZjNmNhMjU2NDk5YjVkMmI4NGE4ZGNlIn19fQ==", "§b§lYoutube", "", getSocialLinks(p, "soc_ytb"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(14, youtube);
 
-        ItemStack twitch = ItemFactory.createHead("Twitch", "e57c6078-0b1f-4f88-976d-977a5834717f", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDZiZTY1ZjQ0Y2QyMTAxNGM4Y2RkZDAxNThiZjc1MjI3YWRjYjFmZDE3OWY0YzFhY2QxNThjODg4NzFhMTNmIn19fQ==", "§bTwitch", "", getSocialLinks(p, "soc_twitch"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack twitch = ItemFactory.createHead("Twitch", "e57c6078-0b1f-4f88-976d-977a5834717f", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDZiZTY1ZjQ0Y2QyMTAxNGM4Y2RkZDAxNThiZjc1MjI3YWRjYjFmZDE3OWY0YzFhY2QxNThjODg4NzFhMTNmIn19fQ==", "§b§lTwitch", "", getSocialLinks(p, "soc_twitch"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(16, twitch);
 
-        ItemStack web = ItemFactory.createHead("Web", "c424243d-0421-4774-8aeb-2ddea957ed57", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTY5MzZkNGYwZDFiOTNmZWY3NzViMWZiZDE5MjgxYjcwYzZmODg0NzViYjVhNDFiZjM3MmMxMmYxZjhhMjIifX19", "§bWeb (vlastni URL)", "", getSocialLinks(p, "soc_web"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack web = ItemFactory.createHead("Web", "c424243d-0421-4774-8aeb-2ddea957ed57", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTY5MzZkNGYwZDFiOTNmZWY3NzViMWZiZDE5MjgxYjcwYzZmODg0NzViYjVhNDFiZjM3MmMxMmYxZjhhMjIifX19", "§b§lWeb (vlastni URL)", "", getSocialLinks(p, "soc_web"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(20, web);
 
-        ItemStack steam = ItemFactory.createHead("Steam", "73bec8bd-bced-44cc-b3ad-3489b846efc4", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzRkYjRhZGZhOWJmNDhmZjVkNDE3MDdhZTM0ZWE3OGJkMjM3MTY1OWZjZDhjZDg5MzQ3NDlhZjRjY2U5YiJ9fX0=", "§bSteam", "", getSocialLinks(p, "soc_steam"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
+        ItemStack steam = ItemFactory.createHead("Steam", "73bec8bd-bced-44cc-b3ad-3489b846efc4", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzRkYjRhZGZhOWJmNDhmZjVkNDE3MDdhZTM0ZWE3OGJkMjM3MTY1OWZjZDhjZDg5MzQ3NDlhZjRjY2U5YiJ9fX0=", "§b§lSteam", "", getSocialLinks(p, "soc_steam"), "", "§eLevym kliknutim zmenis", "§ePravym kliknutim smazes");
         inv.setItem(24, steam);
 
-        ItemStack warn = ItemFactory.createHead("Warning", "76815220-3812-436d-90f8-3264867d7bc0", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWY0NDZhOGY5Mjg0YzYyY2Y4ZDQ5MWZiZGIzMzhmZDM5ZWJiZWJlMzVlOTU5YzJmYzRmNzg2YzY3NTIyZWZiIn19fQ==", "§cVarovani", "§7Vsechny zde uverejnene", "§7finformace jsou verejne dostupne", "§7na tvem webovem profilu", "§7nebo pomoci §a/profil <nick>");
+        ItemStack warn = ItemFactory.createHead("Warning", "76815220-3812-436d-90f8-3264867d7bc0", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWY0NDZhOGY5Mjg0YzYyY2Y4ZDQ5MWZiZGIzMzhmZDM5ZWJiZWJlMzVlOTU5YzJmYzRmNzg2YzY3NTIyZWZiIn19fQ==", "§c§lVarovani", "§7Vsechny zde uverejnene", "§7informace jsou verejne dostupne", "§7na tvem webovem profilu", "§7nebo pomoci §a/profil <nick>");
         inv.setItem(22, warn);
 
         ItemStack zpet = ItemFactory.create(Material.ARROW, (byte) 0, "§cZpet");
@@ -151,7 +142,7 @@ public class Profil implements Listener {
 
         Inventory inv = Bukkit.createInventory(null, 45, "Nastaveni jazyka");
 
-        ItemStack jazyk = ItemFactory.createHead("§aCestina", "9db55454-6114-4bf8-aa27-55419d4890c4", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDgxNTJiNzMzNGQ3ZWNmMzM1ZTQ3YTRmMzVkZWZiZDJlYjY5NTdmYzdiZmU5NDIxMjY0MmQ2MmY0NmU2MWUifX19");
+        ItemStack jazyk = ItemFactory.createHead("§a§lCestina", "9db55454-6114-4bf8-aa27-55419d4890c4", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDgxNTJiNzMzNGQ3ZWNmMzM1ZTQ3YTRmMzVkZWZiZDJlYjY5NTdmYzdiZmU5NDIxMjY0MmQ2MmY0NmU2MWUifX19");
         ItemMeta bhMeta = jazyk.getItemMeta();
         ArrayList<String> bhLore = new ArrayList();
         bhLore.add("§7Zmen si jazyk serveru na Cestinu.");
@@ -163,7 +154,7 @@ public class Profil implements Listener {
         bhMeta.setLore(bhLore);
         jazyk.setItemMeta(bhMeta);
 
-        ItemStack jazyk2 = ItemFactory.createHead("§aSlovencina", "8e225e4c-74bc-43db-9b94-87e0e544dba6", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmM3MmE4YzExNWExZmI2NjlhMjU3MTVjNGQxNWYyMjEzNmFjNGMyNDUyNzg0ZTQ4OTRiM2Q1NmJjNWIwYjkifX19");
+        ItemStack jazyk2 = ItemFactory.createHead("§a§lSlovencina", "8e225e4c-74bc-43db-9b94-87e0e544dba6", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmM3MmE4YzExNWExZmI2NjlhMjU3MTVjNGQxNWYyMjEzNmFjNGMyNDUyNzg0ZTQ4OTRiM2Q1NmJjNWIwYjkifX19");
         ItemMeta bhMeta2 = jazyk2.getItemMeta();
         ArrayList<String> bhLore2 = new ArrayList();
         bhLore2.add("§7Zmen si jazyk serveru na Slovencinu.");
@@ -175,7 +166,7 @@ public class Profil implements Listener {
         bhMeta2.setLore(bhLore2);
         jazyk2.setItemMeta(bhMeta2);
 
-        ItemStack jazyk3 = ItemFactory.createHead("§aEnglish", "3c30484a-76d3-4cfe-88e5-e7599bc9ac4d", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGNhYzk3NzRkYTEyMTcyNDg1MzJjZTE0N2Y3ODMxZjY3YTEyZmRjY2ExY2YwY2I0YjM4NDhkZTZiYzk0YjQifX19");
+        ItemStack jazyk3 = ItemFactory.createHead("§a§lEnglish", "3c30484a-76d3-4cfe-88e5-e7599bc9ac4d", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGNhYzk3NzRkYTEyMTcyNDg1MzJjZTE0N2Y3ODMxZjY3YTEyZmRjY2ExY2YwY2I0YjM4NDhkZTZiYzk0YjQifX19");
         ItemMeta bhMeta3 = jazyk3.getItemMeta();
         ArrayList<String> bhLore3 = new ArrayList();
         bhLore3.add("§7Change your language to English.");
@@ -200,15 +191,23 @@ public class Profil implements Listener {
     }
 
     private void openStatsMenu(Player p) {
-        Inventory inv = Bukkit.createInventory(null, 9 * 3, "Statistiky");
+        Inventory inv = Bukkit.createInventory(null, 54, "Tvoje statistiky");
 
-        ItemStack votes = ItemFactory.create(Material.PAPER, (byte) 0, "§a§lHlasy", "§f",
-                "§7Za tyden: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "week_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "week") + ")",
-                "§7Za mesic: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "month_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "month") + ")",
-                "§7Celkove: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "total_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "total") + ")");
-        inv.setItem(12, votes);
+        SkullMeta headItemMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(Material.SKULL_ITEM);
+        headItemMeta.setOwner(p.getName());
+        headItemMeta.setDisplayName("§a§lTvoje statistiky");
+        ItemStack headItem = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        ArrayList<String> headLore = new ArrayList<>();
+        headLore.add("");
+        headLore.add("§7Tydenni hlasy: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "week_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "week") + ")");
+        headLore.add("§7Mesicni hlasy: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "month_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "month") + ")");
+        headLore.add("§7Celkem hlasu: §f" + Main.getInstance().getSQL().getPlayerProfileDataInt(p, "total_votes") + " §8(#" + Main.getInstance().getSQL().getVotesTopPosition(p, "total") + ")");
+        headItemMeta.setLore(headLore);
+        headItem.setItemMeta(headItemMeta);
 
-        ItemStack bedwars = ItemFactory.create(Material.BED, (byte) 0, "§c§lBedWars", "§f",
+        ItemStack filler = new ItemBuilder(Material.STAINED_GLASS_PANE).setDurability((short)2).setName("§f").hideAllFlags().build();
+
+        ItemStack bedwars = ItemFactory.create(Material.BED, (byte) 0, "§c§lBedWars", "§8Season 0", "",
                 "§7Odehrano: §f" + Main.getInstance().getSQL().getBedwarsPlayerData(p, "games") ,
                 "§7Prohrano: §f" + Main.getInstance().getSQL().getBedwarsPlayerData(p, "loses"),
                 "§7Vyhrano: §f" + Main.getInstance().getSQL().getBedwarsPlayerData(p, "wins"),
@@ -217,7 +216,22 @@ public class Profil implements Listener {
                 "§7Celkove skore: §f" + Main.getInstance().getSQL().getBedwarsPlayerData(p, "score"),
                 "§7",
                 "§7Jsi momentalne §cTOP #" + Main.getInstance().getSQL().getBedwarsTopPosition(p) + "!");
-        inv.setItem(13, bedwars);
+
+        ItemStack back = ItemFactory.create(Material.ARROW, (byte)0, "§cZpet");
+
+        inv.setItem(0, filler);
+        inv.setItem(1, filler);
+        inv.setItem(2, filler);
+        inv.setItem(3, filler);
+        inv.setItem(4, headItem);
+        inv.setItem(5, filler);
+        inv.setItem(6, filler);
+        inv.setItem(7, filler);
+        inv.setItem(8, filler);
+
+        inv.setItem(19, bedwars);
+
+        inv.setItem(49, back);
 
         p.openInventory(inv);
     }
@@ -376,8 +390,12 @@ public class Profil implements Listener {
             }
             e.setCancelled(true);
             p.updateInventory();
-        } if (e.getInventory().getTitle().equals("Statistiky")) {
+        } if (e.getInventory().getTitle().equals("Tvoje statistiky")) {
+            if (e.getSlot() == 49) {
+                openMenu(p);
+            }
             e.setCancelled(true);
+            p.updateInventory();
         }
     }
 
