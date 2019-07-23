030addb32a798d366e90d80e743462521b09dd41
Jacob Bordas <methalicity@gmail.com>
2019-07-23 23:24:28 +0200

    Zmena /link správ

--- src/main/java/cz/wake/lobby/commands/Link_Command.java
+++ src/main/java/cz/wake/lobby/commands/Link_Command.java
@@ -15,21 +15,20 @@ public class Link_Command implements CommandExecutor {
         Player p = (Player) sender;
 
         if (Main.getInstance().getSQL().isConnectedToDiscord(p)) {
-            p.sendMessage("§c§l(!) §cTento ucet jiz je propojen s Discord uctem!");
+            p.sendMessage("§c§l[!] §cTento ucet jiz je propojen s Discord uctem!");
             return true;
         }
 
         if (Main.getInstance().getSQL().hasConnectionRequest(p)) {
-            p.sendMessage("§aTvuj predchazejici kod jeste nevyexpiroval: " + Main.getInstance().getSQL().getConnectionCode(p));
+            p.sendMessage("§eTvuj predchazejici kod jeste nevyexpiroval: " + Main.getInstance().getSQL().getConnectionCode(p));
             return true;
         }
 
         String code = RandomStringUtils.randomAlphanumeric(8).toUpperCase();
         Main.getInstance().getSQL().createConnectionRequest(p, code);
         p.sendMessage("§6Zahajil jsi prepojovani s Discord uctem!");
-        p.sendMessage("§aZajdi na nas Discord server (discord.gg/craftmania) do #bot_prikazy a zadej prikaz ,link " + code);
-        p.sendMessage("§aNasledne se ti prepoji tvuj Minecraft ucet s Discord uctem a budes mit jednoduchsi pristup ke vsem statistikam.");
-        p.sendMessage("§aTvuj kod expiruje za 5 minut.");
+        p.sendMessage("§eZajdi na nas Discord server (discord.gg/craftmania) do #bot_prikazy a zadej prikaz §6,link " + code);
+        p.sendMessage("§eNasledne se ti prepoji tvuj Minecraft ucet s Discord uctem a budes mit jednoduchsi pristup ke vsem statistikam. Tvuj kod expiruje za 5 minut.");
         return true;
     }
 }
