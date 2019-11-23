35acfca1defeff0ebc774006ec6b221cbf69166c
MrWakeCZ <joseff.kral@gmail.com>
2019-11-23 03:57:13 +0100

    Znovu fix klikání na itemframes

--- src/main/java/cz/wake/lobby/listeners/ItemFrameInteractListener.java
+++ src/main/java/cz/wake/lobby/listeners/ItemFrameInteractListener.java
@@ -1,18 +1,34 @@
 package cz.wake.lobby.listeners;
 
 import cz.wake.lobby.Main;
-import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class ItemFrameInteractListener implements Listener {
 
+    @EventHandler(priority = EventPriority.HIGH)
     public void onClick(PlayerInteractEntityEvent event) {
-        if(!event.isCancelled() && event.getRightClicked() instanceof ItemFrame
-                && !((ItemFrame)event.getRightClicked()).getItem().getType().equals(Material.AIR)
+        if(event.getRightClicked() instanceof ItemFrame
+                //&& !((ItemFrame)event.getRightClicked()).getItem().getType().equals(Material.AIR)
                 && !Main.getInstance().isDebug()) {
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void hanging (HangingPlaceEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof ItemFrame) {
+            if (Main.getInstance().isDebug()) {
+                event.setCancelled(false);
+            } else{
+                event.setCancelled(true);
+            }
         }
     }
 }
