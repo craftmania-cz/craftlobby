f4d525159395b16f480893ef8dd8cbe927008108
MrWakeCZ <joseff.kral@gmail.com>
2020-03-22 06:28:36 +0100

    Odebrán Creative 1.12

--- src/main/java/cz/wake/lobby/gui/DeprecatedServersGUI.java
+++ src/main/java/cz/wake/lobby/gui/DeprecatedServersGUI.java
@@ -24,7 +24,7 @@ public class DeprecatedServersGUI implements InventoryProvider {
         contents.set(0,7, ClickableItem.of(new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setName("§c ").build(), e -> {}));
         contents.set(0,8, ClickableItem.of(new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setName("§c ").build(), e -> {}));
 
-        contents.set(0, 4, ClickableItem.of(SkullHeads.createHead(
+        /*contents.set(0, 4, ClickableItem.of(SkullHeads.createHead(
                 "§a§lVyber serveru",
                 "2b7ae64b-f11f-c08a-c3a5-d60bf51f0a91",
                 "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOGUyYzE4YWIzNTk0OWJmOWY5ZTdkNmE2OWI4ODVjY2Q4Y2MyZWZiOTQ3NTk0NmQ3ZDNmYjVjM2ZlZjYxIn19fQ==",
@@ -32,7 +32,7 @@ public class DeprecatedServersGUI implements InventoryProvider {
 
         contents.set(2, 4, ClickableItem.of(new ItemBuilder(Material.WOODEN_AXE).setName("§c§lCreative §e[1.12]").hideAllFlags().setLore("§7Starý Creative na 1.12", "§7vše jak bylo a navždy bude!", "", "§b▸ Kliknutim te portnu").build(), e -> {
             Main.getInstance().getCraftBalancerManager().bypassConnect(player, "creative2");
-        }));
+        }));*/
 
         // Dolni radek
         contents.set(5,0, ClickableItem.of(new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setName("§c ").build(), e -> {}));
