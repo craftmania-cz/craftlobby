2e6e4fd6754816ebf09c59b6b4b175fb65eeafae
MrWakeCZ <joseff.kral@gmail.com>
2020-05-02 04:24:12 +0200

    Oprava ukazování počtu hráčů na Creativu

--- src/main/java/cz/wake/lobby/npc/list/CreativeNPC.java
+++ src/main/java/cz/wake/lobby/npc/list/CreativeNPC.java
@@ -31,7 +31,7 @@ public class CreativeNPC implements IServerNPC {
     public List<String> getHologramLines() {
         List<String> list = new ArrayList<>();
         list.add("§c§lCreative §e[1.15]");
-        list.add("§f" + Main.getInstance().getSQL().getOnlinePlayers("creative") +" §7hráčů");
+        list.add("§f" + Main.getInstance().getSQL().getOnlinePlayers("creative2") +" §7hráčů");
         return list;
     }
 
