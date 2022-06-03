bf804214bf58f74bf78e404585ecbfef605151c2
MrWakeCZ <joseff.kral@gmail.com>
2022-06-03 03:12:49 +0200

    Remove: Vanilla Anarchy & Prison

--- src/main/java/cz/wake/lobby/gui/ServerSelectorGUI.java
+++ src/main/java/cz/wake/lobby/gui/ServerSelectorGUI.java
@@ -35,7 +35,7 @@ public class ServerSelectorGUI implements InventoryProvider {
                 .build(), (clickEvent) -> Main.getInstance().getCraftBalancerManager().bypassConnect(player, "skyblock")));
 
         contents.set(0, 3, ClickableItem.of(new ItemBuilder(Material.LAVA_BUCKET)
-                .setName(ChatColor.of("#F4A460") + "§lCreative §8[1.17]")
+                .setName(ChatColor.of("#F4A460") + "§lCreative §8[1.18]")
                 .setLore(ChatColor.of("#7D7D7D") + "Pozemky, WorldEdit", "", "§7Svět, ve kterém můžeš prakticky", "§7dělat co chceš. Stavět, budovat,", "§7nebo se prostě zlepšovat.", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().getCraftBalancerManager().bypassConnect(player, "creative")));
@@ -45,18 +45,6 @@ public class ServerSelectorGUI implements InventoryProvider {
                 .setLore(ChatColor.of("#7D7D7D") + "Ochrana chunků, PvP, Bez ekonomiky", "", "§7Prakticky Survival ale bez Ekonomiky.", "§7Dokážeš přežít bez pomoci", "§7a v klasickém světě?", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().getCraftBalancerManager().bypassConnect(player, "vanilla")));
-
-        contents.set(1, 2, ClickableItem.of(new ItemBuilder(Material.IRON_BARS)
-                .setName(ChatColor.of("#9340c2") + "§lPrison §8[1.16]")
-                .setLore(ChatColor.of("#7D7D7D") + "Economy, Mining, Prestiges", "", "§7Prison je zpět a s ním,", "§7i tuna kopání a OP enchanty.", "§7Dokážeš se dostat co nejdál,", "§7a získat tak titul vítěze?", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
-                .hideAllFlags()
-                .build(), (clickEvent) -> Main.getInstance().getCraftBalancerManager().bypassConnect(player, "prison")));
-
-        contents.set(1, 3, ClickableItem.of(new ItemBuilder(Material.REDSTONE)
-                .setName(ChatColor.of("#CD0000") + "§lVanilla: Anarchy §8[1.17]")
-                .setLore(ChatColor.of("#7D7D7D") + "Vanilla, žádná ochrana", "", "§7Tento server je určený pro náročné", "§7Vanilla hráče, kteří nechtějí ochrany", "§7a chtějí dělat bordel!", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
-                .hideAllFlags()
-                .build(), (clickEvent) -> Main.getInstance().getCraftBalancerManager().bypassConnect(player, "anarchy")));
 
         contents.set(0, 6, ClickableItem.of(new ItemBuilder(Material.CAKE)
                 .setName(ChatColor.of("#EE30A7") + "§lEvent Server")
