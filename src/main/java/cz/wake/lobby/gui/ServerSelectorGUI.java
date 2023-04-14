011a41f93c4fc84ef0da66ffe5a99f0a51ee0e43
MrWakeCZ <joseff.kral@gmail.com>
2023-04-14 23:40:18 +0200

    Update: Vrácení Survivalu 1.17 do hlavních serverů

--- src/main/java/cz/wake/lobby/gui/ServerSelectorGUI.java
+++ src/main/java/cz/wake/lobby/gui/ServerSelectorGUI.java
@@ -19,31 +19,31 @@ public class ServerSelectorGUI implements InventoryProvider {
     @Override
     public void init(Player player, InventoryContents contents) {
 
-        contents.set(1, 2, ClickableItem.of(new ItemBuilder(Material.DIAMOND_PICKAXE)
+        contents.set(0, 1, ClickableItem.of(new ItemBuilder(Material.DIAMOND_PICKAXE)
                 .setName(ChatColor.of("#00CDCD") + "§lSurvival §8[1.17]")
                 .setLore(ChatColor.of("#7D7D7D") + "Economy, PvP, McMMO", "", "§7Survival server založený na ekonomice,", "§7s shopy a farmami. Né každý ale", "§7může být ten nejbohatší.", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().sendToServer(player, "survival2")));
 
-        contents.set(0, 2, ClickableItem.of(new ItemBuilder(Material.HEART_OF_THE_SEA)
+        contents.set(0, 3, ClickableItem.of(new ItemBuilder(Material.HEART_OF_THE_SEA)
                 .setName(ChatColor.of("#3D9140") + "§lSkyblock §8[1.19]")
                 .setLore(ChatColor.of("#7D7D7D") + "Economy, PvP", "", "§7Dokážeš z prázdného ostrova postavit", "§7něco, co bude mít hodnotu?", "§7Jedině tak přežiješ...", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().sendToServer(player, "skyblock")));
 
-        contents.set(0, 3, ClickableItem.of(new ItemBuilder(Material.LAVA_BUCKET)
+        contents.set(1, 1, ClickableItem.of(new ItemBuilder(Material.LAVA_BUCKET)
                 .setName(ChatColor.of("#F4A460") + "§lCreative §8[1.19]")
                 .setLore(ChatColor.of("#7D7D7D") + "Pozemky, WorldEdit", "", "§7Svět, ve kterém můžeš prakticky", "§7dělat co chceš. Stavět, budovat,", "§7nebo se prostě zlepšovat.", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().sendToServer(player, "creative")));
 
-        contents.set(1, 1, ClickableItem.of(new ItemBuilder(Material.IRON_NUGGET).setCustomModelData(100014)
+        contents.set(1, 2, ClickableItem.of(new ItemBuilder(Material.IRON_NUGGET).setCustomModelData(100014)
                 .setName(ChatColor.of("#EEC900") + "§lVanilla: Lands §8[1.19]")
                 .setLore(ChatColor.of("#7D7D7D") + "Ochrana chunků, PvP, Bez ekonomiky", "", "§7Prakticky Survival ale bez Ekonomiky.", "§7Dokážeš přežít bez pomoci", "§7a v klasickém světě?", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
                 .build(), (clickEvent) -> Main.getInstance().sendToServer(player, "vanilla")));
 
-        contents.set(0, 1, ClickableItem.of(new ItemBuilder(Material.NETHERITE_SWORD)
+        contents.set(0, 2, ClickableItem.of(new ItemBuilder(Material.NETHERITE_SWORD)
                 .setName(ChatColor.of("#00CDCD") + "§lSurvival §8[1.19]")
                 .setLore(ChatColor.of("#7D7D7D") + "Ochrana chunků, PvP, Hard Ekonomika", "", "§7Zcela nový Survival.", "§7S těžší ekonomikou, custom", "§7bloky a tunou vylepšení...", "", ChatColor.of("#FFD39B") + "Klikni pro připojení na server")
                 .hideAllFlags()
